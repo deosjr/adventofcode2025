a4bb8908f83fa2738e8ed2be5fc5241b70f3d041
Sjoerd Dost <deosjr@gmail.com>
2025-12-01 07:04:10 +0100

    day01p2

--- 01/day01.go
+++ 01/day01.go
@@ -9,20 +9,31 @@ func posmod(n, m int64) int64 {
 }
 
 func main() {
+	//lib.Test()
 	var dial int64 = 50
-	p1 := 0
+	var p1, p2 int64 = 0, 0
 	lib.ReadFileByLine(1, func(line string) {
 		n := lib.MustParseInt(line[1:]) // note utf8!
+		p2 += n / 100
 		n = posmod(n, 100)
+		prev := dial
 		switch line[0] {
 		case 'L':
 			dial = posmod(dial - n, 100)
+			if prev != 0 && n > prev && dial != 0 {
+				p2++
+			}
 		case 'R':
 			dial = posmod(dial + n, 100)
+			if prev != 0 && dial < prev && dial != 0 {
+				p2++
+			}
 		}
 		if dial == 0 {
 			p1++
+			p2++
 		}
 	})
 	lib.WritePart1("%d", p1)
+	lib.WritePart2("%d", p2)
 }
