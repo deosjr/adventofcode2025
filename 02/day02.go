b1ff400aec39f874c33d5260f5fe2d5cbf340cc7
Sjoerd Dost <deosjr@gmail.com>
2025-12-02 10:03:26 +0100

    day02p2

--- 02/day02.go
+++ 02/day02.go
@@ -11,9 +11,15 @@ type IDrange struct {
 	fromHigh, toHigh int64
 	fromLow, toLow int64
 	pow int64
+	evenLen int
+	unevenLen int
+	unevenFrom, unevenTo int64
 }
 
 // observation: ranges length diff between from/to is max 1
+// observation: 10 digits is the max for any number
+// p1 checks all even repetitions, p2 adds uneven for length 3, 5 and 7
+// 3 has to be checked in ranges of len 3, 6 and 9, 5 in 5 and 10, and 7 in 7 only
 func parseIDrange(s string) IDrange {
 	split := strings.Split(strings.TrimSpace(s), "-")
 	from := lib.MustParseInt(split[0])
@@ -22,16 +28,29 @@ func parseIDrange(s string) IDrange {
 	fromLen, toLen := len(split[0]), len(split[1])
 	fromEven, toEven := fromLen % 2 == 0, toLen % 2 == 0
 	if !fromEven && !toEven {
+		r.unevenFrom = from
+		r.unevenTo = to
+		r.unevenLen = fromLen
 		return r
 	}
 	if !fromEven {
+		r.unevenFrom = from
+		r.unevenTo = int64(math.Pow10(fromLen)) - 1
+		r.unevenLen = fromLen
 		from = int64(math.Pow10(fromLen))
-		fromLen++
+		r.evenLen = toLen
 	}
 	if !toEven {
+		r.unevenFrom = int64(math.Pow10(fromLen))
+		r.unevenTo = to
+		r.unevenLen = toLen
 		to = int64(math.Pow10(fromLen)) - 1
+		r.evenLen = fromLen
 	}
-	cutOff := int64(math.Pow10(fromLen/2))
+	if fromEven && toEven {
+		r.evenLen = fromLen
+	}
+	cutOff := int64(math.Pow10(r.evenLen/2))
 	r.fromHigh = from / cutOff
 	r.toHigh = to / cutOff
 	r.fromLow = from % cutOff
@@ -44,9 +63,9 @@ func pasteNum(high, low, pow int64) int64 {
 	return high * pow + low
 }
 
-func (r IDrange) findInvalidIDs() []int64 {
+func (r IDrange) findEvenInvalidIDs() []int64 {
 	ids := []int64{}
-	if r.fromHigh == 0 {
+	if r.evenLen == 0 {
 		return ids
 	}
 	if r.fromHigh == r.toHigh {
@@ -64,24 +83,169 @@ func (r IDrange) findInvalidIDs() []int64 {
 	if r.toLow >= r.toHigh {
 		ids = append(ids, pasteNum(r.toHigh, r.toHigh, r.pow))
 	}
-	lib.WritePart2("%v", r)
-	lib.WritePart2("%d", ids)
+	return ids
+}
+
+func (r IDrange) find3In3() []int64 {
+	ids := []int64{}
+	for _, n := range []int64{ 111, 222, 333, 444, 555, 666, 777, 888, 999 } {
+		if n < r.unevenFrom || n > r.unevenTo {
+			continue
+		}
+		ids = append(ids, n)
+	}
+	return ids
+}
+
+func (r IDrange) find3In6() []int64 {
+	ids := []int64{}
+	fromHigh := r.fromHigh / 10
+	toHigh := r.toHigh / 10
+	targetFunc := func(n int64) int64 {
+		return n * 10000 + n * 100 + n
+	}
+	from := pasteNum(r.fromHigh, r.fromLow, r.pow)
+	to := pasteNum(r.toHigh, r.toLow, r.pow)
+
+	if fromHigh == toHigh {
+		target := targetFunc(fromHigh)
+		if target >= from && target <= to {
+			ids = append(ids, target)
+		}
+		return ids
+	}
+	if target := targetFunc(fromHigh); target >= from {
+		ids = append(ids, target)
+	}
+	for n := fromHigh+1; n<toHigh; n++ {
+		ids = append(ids, targetFunc(n))
+	}
+	if target := targetFunc(toHigh); target <= to {
+		ids = append(ids, target)
+	}
+	return ids
+}
+
+func (r IDrange) find3In9() []int64 {
+	ids := []int64{}
+	fromHigh := r.unevenFrom / 1000000
+	toHigh := r.unevenTo / 1000000
+	targetFunc := func(n int64) int64 {
+		return n * 1000000 + n * 1000 + n
+	}
+	from := r.unevenFrom
+	to := r.unevenTo
+
+	if fromHigh == toHigh {
+		target := targetFunc(fromHigh)
+		if target >= from && target <= to {
+			ids = append(ids, target)
+		}
+		return ids
+	}
+	if target := targetFunc(fromHigh); target >= from {
+		ids = append(ids, target)
+	}
+	for n := fromHigh+1; n<toHigh; n++ {
+		ids = append(ids, targetFunc(n))
+	}
+	if target := targetFunc(toHigh); target <= to {
+		ids = append(ids, target)
+	}
+	return ids
+}
+
+func (r IDrange) find5In5() []int64 {
+	ids := []int64{}
+	for _, n := range []int64{ 11111, 22222, 33333, 44444, 55555, 66666, 77777, 88888, 99999 } {
+		if n < r.unevenFrom || n > r.unevenTo {
+			continue
+		}
+		ids = append(ids, n)
+	}
+	return ids
+}
+
+func (r IDrange) find5In10() []int64 {
+	ids := []int64{}
+	subr := IDrange{
+		evenLen: 4,
+		fromHigh: r.fromHigh / 1000,
+		fromLow: (r.fromHigh / 10) % 100,
+		toHigh: r.toHigh / 1000,
+		toLow: (r.toHigh / 10) % 100,
+		pow: 100,
+	}
+	for _, dupe := range subr.findEvenInvalidIDs() {
+		n := dupe % 100
+		target := dupe * 1000000 + dupe * 100 + n
+		if target < r.from || target > r.to {
+			continue
+		}
+		ids = append(ids, target)
+	}
+	return ids
+}
+
+func (r IDrange) find7In7() []int64 {
+	ids := []int64{}
+	for _, n := range []int64{ 1111111, 2222222, 3333333, 4444444, 5555555, 6666666, 7777777, 8888888, 9999999 } {
+		if n < r.unevenFrom || n > r.unevenTo {
+			continue
+		}
+		ids = append(ids, n)
+	}
 	return ids
 }
 
 func main() {
-	//lib.Test()
 	in := lib.ReadFile(2)
 	ranges := []IDrange{}
 	for _, s := range strings.Split(in, ",") {
 		ranges = append(ranges, parseIDrange(s))
 	}
-	var p1 int64
+	var p1, p2 int64
 	for _, r := range ranges {
-		invalids := r.findInvalidIDs()
-		for _, n := range invalids {
-			p1 += n
+		p2set := map[int64]struct{}{}
+		invalids := r.findEvenInvalidIDs()
+		for _, id := range invalids {
+			p1 += id
+			p2set[id] = struct{}{}
+		}
+		if r.unevenLen == 3 {
+			for _, id := range r.find3In3() {
+				p2set[id] = struct{}{}
+			}
+		}
+		if r.evenLen == 6 {
+			for _, id := range r.find3In6() {
+				p2set[id] = struct{}{}
+			}
+		}
+		if r.unevenLen == 9 {
+			for _, id := range r.find3In9() {
+				p2set[id] = struct{}{}
+			}
+		}
+		if r.unevenLen == 5 {
+			for _, id := range r.find5In5() {
+				p2set[id] = struct{}{}
+			}
+		}
+		if r.evenLen == 10 {
+			for _, id := range r.find5In10() {
+				p2set[id] = struct{}{}
+			}
+		}
+		if r.unevenLen == 7 {
+			for _, id := range r.find7In7() {
+				p2set[id] = struct{}{}
+			}
+		}
+		for id, _ := range p2set {
+			p2 += id
 		}
 	}
 	lib.WritePart1("%d", p1)
+	lib.WritePart2("%d", p2)
 }
