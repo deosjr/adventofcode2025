4800f370b202d2fd47f3063e7eff20a3a45f96b8
Sjoerd Dost <deosjr@gmail.com>
2025-12-03 08:26:16 +0100

    day03p2

--- 03/day03.go
+++ 03/day03.go
@@ -1,30 +1,31 @@
 package main
 
 import (
-	"fmt"
 	"github.com/deosjr/adventofcode2025/lib"
 )
 
-func part1(line string) int64 {
+func joltage(line string, n int) int64 {
 	length := len(line)
-	dec, uni := line[length-2], line[length-1]
-	for i:=length-3; i >=0; i-- {
+	list := []byte(line[length-n:])
+Loop:
+	for i:=length-n-1; i >=0; i-- {
 		c := line[i]
-		if c < dec {
-			continue
-		}
-		dec, c = c, dec
-		if c > uni {
-			uni = c
+		for j:=0; j<n; j++ {
+			if c < list[j] {
+				continue Loop
+			}
+			list[j], c = c, list[j]
 		}
 	}
-	return lib.MustParseInt(fmt.Sprintf("%c%c", dec, uni))
+	return lib.MustParseInt(string(list))
 }
 
 func main() {
-	var p1 int64
+	var p1, p2 int64
 	lib.ReadFileByLine(3, func(line string) {
-		p1 += part1(line)
+		p1 += joltage(line, 2)
+		p2 += joltage(line, 12)
 	})
 	lib.WritePart1("%d", p1)
+	lib.WritePart2("%d", p2)
 }
