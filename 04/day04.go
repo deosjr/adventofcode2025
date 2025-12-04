cb0eb637382682f94eb485bb8214b83e0f19a26b
Sjoerd Dost <deosjr@gmail.com>
2025-12-04 08:19:19 +0100

    day04p2

--- 04/day04.go
+++ 04/day04.go
@@ -6,33 +6,63 @@ type coord struct {
 	x, y int
 }
 
+func getNeighbours(grid map[coord]bool) map[coord]int {
+	neighbours := map[coord]int{}
+	for c := range grid {
+		x, y := c.x, c.y
+		neighbours[coord{x-1, y}] += 1
+		neighbours[coord{x+1, y}] += 1
+		neighbours[coord{x, y-1}] += 1
+		neighbours[coord{x, y+1}] += 1
+		neighbours[coord{x-1, y-1}] += 1
+		neighbours[coord{x-1, y+1}] += 1
+		neighbours[coord{x+1, y-1}] += 1
+		neighbours[coord{x+1, y+1}] += 1
+	}
+	return neighbours
+}
+
+func reduceGrid(grid map[coord]bool) map[coord]bool {
+	newgrid := map[coord]bool{}
+	neighbours := getNeighbours(grid)
+	for c := range grid {
+		if neighbours[c] < 4 {
+			continue
+		}
+		newgrid[c] = true
+	}
+	return newgrid
+}
+
 func main() {
-	//lib.Test()
 	y := 0
 	grid := map[coord]bool{}
-	neighbours := map[coord]int{}
 	lib.ReadFileByLine(4, func(line string) {
 		for x, c := range line {
 			if c == '.' {
 				continue
 			}
 			grid[coord{x, y}] = true
-			neighbours[coord{x-1, y}] += 1
-			neighbours[coord{x+1, y}] += 1
-			neighbours[coord{x, y-1}] += 1
-			neighbours[coord{x, y+1}] += 1
-			neighbours[coord{x-1, y-1}] += 1
-			neighbours[coord{x-1, y+1}] += 1
-			neighbours[coord{x+1, y-1}] += 1
-			neighbours[coord{x+1, y+1}] += 1
 		}
 		y++
 	})
 	p1 := 0
+	neighbours := getNeighbours(grid)
 	for c := range grid {
 		if neighbours[c] < 4 {
 			p1++
 		}
 	}
 	lib.WritePart1("%d", p1)
+	p2 := 0
+	for {
+		g := reduceGrid(grid)
+		diff := len(grid) - len(g)
+		if diff == 0 {
+			break
+		}
+		p2 += diff
+		grid = g
+	}
+	lib.WritePart2("%d", p2)
 }
