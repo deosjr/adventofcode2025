ae557713085704ec8025d8e462708ba169769ba6
Sjoerd Dost <deosjr@gmail.com>
2025-12-05 08:53:54 +0100

    day05p2

--- 05/day05.go
+++ 05/day05.go
@@ -1,12 +1,45 @@
 package main
 
 import (
+	"sort"
 	"strings"
 	"github.com/deosjr/adventofcode2025/lib"
 )
 
 type IDrange struct {
 	from, to int64
+}
+
+func (r IDrange) overlap(o IDrange) (IDrange, bool) {
+	if r.from > o.to || r.to < o.from {
+		return IDrange{}, false
+	}
+	from := r.from
+	if o.from < from {
+		from = o.from
+	}
+	to := r.to
+	if o.to > to {
+		to = o.to
+	}
+	return IDrange{from:from, to:to}, true
+}
+
+func collapseRanges(list []IDrange) []IDrange {
+	sort.SliceStable(list, func(i, j int) bool { return list[i].from < list[j].from })
+	newlist := []IDrange{}
+	r := list[0]
+	for _, o := range list[1:] {
+		nr, ok := r.overlap(o)
+		if ok {
+			r = nr
+			continue
+		}
+		newlist = append(newlist, r)
+		r = o
+	}
+	newlist = append(newlist, r)
+	return newlist
 }
 
 func main() {
@@ -21,6 +54,7 @@ func main() {
 	for _, s := range strings.Split(input[1], "\n") {
 		ids = append(ids, lib.MustParseInt(s))
 	}
+	ranges = collapseRanges(ranges)
 	
 	var p1 int64
 	for _, id := range ids {
@@ -34,4 +68,10 @@ func main() {
 	}
 	
 	lib.WritePart1("%d", p1)
+
+	var p2 int64
+	for _, r := range ranges {
+		p2 += r.to - r.from + 1
+	}
+	lib.WritePart2("%d", p2)
 }
