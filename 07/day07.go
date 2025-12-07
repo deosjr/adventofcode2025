7e54b2ea8fc6878372d8fb119b3451e989374d82
Sjoerd Dost <deosjr@gmail.com>
2025-12-07 09:34:45 +0100

    day07p2

--- 07/day07.go
+++ 07/day07.go
@@ -6,26 +6,35 @@ import (
 )
 
 func main() {
-	var fringe map[int]struct{}
+	var fringe map[int]int
 	split := 0
 	lib.ReadFileByLine(7, func(line string) {
 		if fringe == nil {
 			start := strings.IndexRune(line, 'S')
-			fringe = map[int]struct{}{start:struct{}{}}
+			fringe = map[int]int{start:1}
 			return
 		}
+		newfringe := map[int]int{}
 		for i, c := range line {
+			v, ok := fringe[i]
+			if !ok {
+				continue
+			}
 			if c == '.' {
+				newfringe[i] += v
 				continue
 			}
-			if _, ok := fringe[i]; !ok {
-				continue
-			}
-			delete(fringe, i)
-			fringe[i-1] = struct{}{}
-			fringe[i+1] = struct{}{}
+			newfringe[i-1] += v
+			newfringe[i+1] += v
 			split++
 		}
+		fringe = newfringe
 	})
 	lib.WritePart1("%d", split)
+	
+	sum := 0
+	for _, v := range fringe {
+		sum += v
+	}
+	lib.WritePart2("%d", sum)
 }
