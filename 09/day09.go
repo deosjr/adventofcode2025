b2b82cbe12274d6608899acfcf1f983441ab7adc
Sjoerd Dost <deosjr@gmail.com>
2025-12-09 12:36:47 +0100

    day09p2

--- 09/day09.go
+++ 09/day09.go
@@ -8,6 +8,7 @@ import (
 
 func main() {
 	var tiles []image.Point
+	var rects []image.Rectangle
 	var p1 int
 	lib.ReadFileByLine(9, func(line string) {
 		split := strings.Split(line, ",")
@@ -15,13 +16,46 @@ func main() {
 		y := lib.MustParseInt(split[1])
 		tile := image.Pt(int(x), int(y))
 		for _, t := range tiles {
-			r := image.Rect(tile.X, tile.Y, t.X, t.Y)
-			area := (r.Size().X + 1) * (r.Size().Y + 1)
+			r := image.Rect(tile.X, tile.Y, t.X+1, t.Y+1)
+			area := r.Size().X * r.Size().Y
 			if area > p1 {
 				p1 = area
 			}
+			rects = append(rects, r)
 		}
 		tiles = append(tiles, tile)
 	})
 	lib.WritePart1("%d", p1)
+
+	var lines []image.Rectangle
+	from := tiles[len(tiles)-1]
+	for _, to := range tiles {
+		r := image.Rect(from.X, from.Y, to.X, to.Y)
+		if from.X == to.X {
+			r.Max.X += 1
+		} else {
+			r.Max.Y += 1
+		}
+		lines = append(lines, r)
+		from = to
+	}
+	
+	var p2 int
+Loop:
+	for _, r := range rects {
+		area := r.Size().X * r.Size().Y
+		if p2 >= area {
+			continue
+		}
+		inset := r.Inset(1)
+		for _, line := range lines {
+			if inset.Overlaps(line) {
+				continue Loop
+			}
+		}
+		if area > p2 {
+			p2 = area
+		}
+	}
+	lib.WritePart2("%d", p2)
 }
